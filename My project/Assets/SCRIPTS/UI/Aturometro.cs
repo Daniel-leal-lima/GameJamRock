0aab62a32c45cfbd15587a505b21ae055b923b11
Daniel-leal-lima <daniel.leal.lima@hotmail.com>
2022-08-07 19:29:01 -0300

    acaba musica

--- My project/Assets/SCRIPTS/UI/Aturometro.cs
+++ My project/Assets/SCRIPTS/UI/Aturometro.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Aturometro : MonoBehaviour
 {
@@ -26,7 +27,10 @@ public class Aturometro : MonoBehaviour
         {
             MomentoFunc(momento[momentoAtual]);
         }
-        //else ACABAR JOGO
+        else // acaba jogo
+        {
+            SceneManager.LoadScene("Obrigado");
+        }
     }
 
     void MomentoFunc (Momento momento)
